1080165d8fabcfadcfe5a7ff6edb03106ab29e58
tranvanthien <tranvanthien@gmail.com>
2014-06-16 09:10:38 +0700

    update

--- src/com/example/takhom/LoginFragment.java
+++ src/com/example/takhom/LoginFragment.java
@@ -1,18 +1,214 @@
 package com.example.takhom;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class LoginFragment extends SherlockFragment {
 
+	private static Handler mHandler = new Handler();
+	private ProgressDialog progressDialog;
+	private EditText email, pin;
+	private TextView forgot_password_text;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.login_fragment, container, false);
+
+		Button login_button = (Button)rootView.findViewById(R.id.login_button);
+		email = (EditText)rootView.findViewById(R.id.email);
+		email.setText("tranvanthien113@gmail.com");
+		
+		forgot_password_text = (TextView)rootView.findViewById(R.id.forgot_password_text);
+		pin = (EditText)rootView.findViewById(R.id.pin);
+
+		login_button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				progressDialog = ProgressDialog.show(getActivity(), "", "Login ...");
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						login(email.getText().toString(), pin.getText().toString());
+					}}).start();
+			}
+		});
+		
+		forgot_password_text.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				forgotPassword();
+			}
+		});
 		return rootView;
+	}
+
+	public void login(final String username, final String password) {
+		HttpParams httpParameters = new BasicHttpParams();
+	    int timeoutConnection = 10000;
+	    HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+	    int timeoutSocket = 10000;
+	    HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+		HttpClient client = new DefaultHttpClient(httpParameters);  
+		HttpPost post = new HttpPost("https://www.takehome.us/develop/login/"); 
+		post.setHeader("Content-type", "application/json");
+
+		try {
+			JSONObject obj = new JSONObject();
+			obj.put("email", username);
+			obj.put("password", password);
+
+			post.setEntity(new StringEntity(obj.toString(), "UTF-8"));
+			HttpResponse response = client.execute(post);
+			// CONVERT RESPONSE TO STRING
+			final String result = EntityUtils.toString(response.getEntity());
+			System.out.println("result="+result);
+			final JSONObject jsonObj = new JSONObject(result);
+			final int code = jsonObj.getInt("error_code");
+			final String message = jsonObj.getString("message");
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					progressDialog.dismiss();
+					if(code == 0) {
+						try {
+							final JSONObject jsonObj = new JSONObject(message);
+							final String token = jsonObj.getString("token");
+							System.out.println("token="+token);
+							SharedPreferences mPrefs = getActivity().getSharedPreferences("LSprefs",0);
+							SharedPreferences.Editor editor = mPrefs.edit();
+							editor.putString("TOKEN", token);
+							editor.commit();
+							MainActivity.isLogin = true;
+
+							((MainActivity)getActivity()).click("Home");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					} else {
+						try {
+							AlertDialog alertDialog = new AlertDialog.Builder(
+									getActivity()).create();
+							alertDialog.setTitle("Error: " +code);
+							alertDialog.setMessage(message);
+
+							// Setting Icon to Dialog
+							alertDialog.setIcon(R.drawable.ic_launcher);
+							alertDialog.show();
+						} catch (Exception e) {
+
+						}
+					}
+				}});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					progressDialog.dismiss();
+				}});
+		}
+	}
+	
+	public void forgotPassword() {
+		LayoutInflater li = LayoutInflater.from(getActivity());
+		View myView = li.inflate(R.layout.forgot_password, null);
+
+		AlertDialog.Builder cDialog = new AlertDialog.Builder(getActivity());
+		cDialog.setTitle("Reset your password");
+		cDialog.setIcon(R.drawable.ic_launcher);
+		cDialog.setView(myView);
+		//AlertDialog tmp = cDialog.create();
+		final EditText email = (EditText) myView.findViewById(R.id.email);
+		email.setText("tranvanthien113@gmail.com");
+		cDialog.setPositiveButton("Ok", new   DialogInterface.OnClickListener() {
+			  @Override
+			  public void onClick(DialogInterface dialog, int which) {
+				  dialog.cancel();
+				  //progressDialog = ProgressDialog.show(getActivity(), "", "Sending mail ...");
+					new Thread(new Runnable() {
+						@Override
+						public void run() {
+							forgotpass(email.getText().toString());
+						}}).start();
+			  }
+			});
+			cDialog.setNegativeButton("Cancel", new   DialogInterface.OnClickListener() {
+				  @Override
+				  public void onClick(DialogInterface dialog, int which) {
+				    //here the code to retrieve dialog
+				  }
+				});
+		cDialog.create();
+		cDialog.show();
+	}
+	
+	public void forgotpass(final String username) {
+		HttpClient client = new DefaultHttpClient();  
+		HttpPost post = new HttpPost("https://www.takehome.us/develop/resetrequest/"); 
+		post.setHeader("Content-type", "application/json");
+
+		try {
+			JSONObject obj = new JSONObject();
+			obj.put("email", username);
+
+			post.setEntity(new StringEntity(obj.toString(), "UTF-8"));
+			HttpResponse response = client.execute(post);
+			// CONVERT RESPONSE TO STRING
+			final String result = EntityUtils.toString(response.getEntity());
+			final JSONObject jsonObj = new JSONObject(result);
+			final int code = jsonObj.getInt("error_code");
+			final String message = jsonObj.getString("message");
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					//progressDialog.dismiss();
+					
+					AlertDialog alertDialog = new AlertDialog.Builder(
+							getActivity()).create();
+					alertDialog.setTitle("Code:" +code);
+					alertDialog.setMessage(message);
+
+					// Setting Icon to Dialog
+					alertDialog.setIcon(R.drawable.ic_launcher);
+					alertDialog.show();
+
+				}});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					//progressDialog.dismiss();
+				}});
+		}
 	}
 
 }
--- src/com/example/takhom/MainActivity.java
+++ src/com/example/takhom/MainActivity.java
@@ -1,82 +1,70 @@
 package com.example.takhom;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.SubMenu;
 
 public class MainActivity extends SherlockFragmentActivity {
-    private TextView mSelected;
-    private String[] mLocations;
-    public static int THEME = R.style.Theme_Sherlock_Light;
-    public LoginFragment loginFragment = null;
-    public TestFragment testFragment = null;
+	private TextView mSelected;
+	private String[] mLocations;
+	public static int THEME = R.style.Theme_Sherlock_Light;
+	public LoginFragment loginFragment = null;
+	public TestFragment testFragment = null;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        setTheme(THEME); //Used for theme switching in samples
-        super.onCreate(savedInstanceState);
-        
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        if(loginFragment == null)
-    		loginFragment = new LoginFragment();
-    	ft.replace(android.R.id.content, loginFragment);
-    	ft.commit();
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-    	if(item.getTitle().equals("Home")) {
-    		//if(loginFragment == null)
-        		loginFragment = new LoginFragment();
-        	ft.replace(android.R.id.content, loginFragment);
-    	} else if (item.getTitle().equals("Archives")) {
-    		//if(testFragment == null)
-        		testFragment = new TestFragment();
-        	ft.replace(android.R.id.content, testFragment);
-    	} else if (item.getTitle().equals("Help")) {
-    		ft.replace(android.R.id.content, new HomeFragment());
-    	}
-    	ft.commit();
-        //Toast.makeText(this, "Got click: " + item.getTitle() , Toast.LENGTH_SHORT).show();
-        return true;
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+	public static boolean isLogin = false;
 
-        SubMenu subMenu1 = menu.addSubMenu("Action Item");
-        subMenu1.add("Home");
-        subMenu1.add("Archives");
-        subMenu1.add("Help");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setTheme(THEME); //Used for theme switching in samples
+		super.onCreate(savedInstanceState);
 
-        MenuItem subMenu1Item = subMenu1.getItem();
-        subMenu1Item.setIcon(R.drawable.abs__ic_menu_moreoverflow_normal_holo_light);
-        subMenu1Item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        
-        menu.add("Save")
-        .setIcon(R.drawable.img_girl)
-        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		if(loginFragment == null)
+			loginFragment = new LoginFragment();
+		ft.replace(android.R.id.content, loginFragment);
+		ft.commit();
+	}
 
-//        SubMenu subMenu2 = menu.addSubMenu("Overflow Item");
-//        subMenu2.add("These");
-//        subMenu2.add("Are");
-//        subMenu2.add("Sample");
-//        subMenu2.add("Items");
-//
-//        MenuItem subMenu2Item = subMenu2.getItem();
-//        subMenu2Item.setIcon(R.drawable.ic_compose);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		click(item.getTitle().toString());
+		return true;
+	}
 
-        return super.onCreateOptionsMenu(menu);
-    }
+	public void click(String title) {
+		if(!isLogin) return;
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		if(title.equals("Home")) {
+			ft.replace(android.R.id.content, new HomeFragment());
+		} else if (title.equals("Archives")) {
+			ft.replace(android.R.id.content, new TestFragment());
+		} else if (title.equals("Help")) {
+			//ft.replace(android.R.id.content, new HomeFragment());
+		}
+		ft.commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		SubMenu subMenu1 = menu.addSubMenu("Action Item");
+		subMenu1.add("Home");
+		subMenu1.add("Archives");
+		subMenu1.add("Help");
+
+		MenuItem subMenu1Item = subMenu1.getItem();
+		subMenu1Item.setIcon(R.drawable.abs__ic_menu_moreoverflow_normal_holo_light);
+		subMenu1Item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+		menu.add("Save")
+		.setIcon(R.drawable.img_girl)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		return super.onCreateOptionsMenu(menu);
+	}
 }
